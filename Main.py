defa3f23f734782489df6412413e0ca54a08719c
Casperbkkr <casperbkkr@gmail.com>
2023-03-14 16:08:44 +0100

    trying to profile

--- Main.py
+++ Main.py
@@ -1,18 +1,39 @@
 #!/usr/bin/env python
-import pandas as pd
+import matplotlib.pyplot as plt
 import polars as pl
 from Tree import Tree
 from Forest import Forest
-df = pl.read_csv('Iris.csv')
-df.drop_in_place('Id')
-#F = Forest(df, n_trees=5, target="Species")
-df_inp = df[58]
-#out = F.Predict(df_inp)
 
-df2 = pl.read_csv("exams.csv")
-F2 = Forest(df2, n_trees=3, target="writing score")
+df = pl.read_csv('exams.csv')
+#df.drop_in_place('Id')
 
-out2 = F2.Predict(df2[50])
-print(out2)
-print(df2.row(50))
+
+df_sh = df.sample(n=df.shape[0], with_replacement=False, shuffle=True)
+#print(df_sh)
+training_size = 30
+test_size = df_sh.shape[0] - training_size
+df_tr = df_sh[:training_size]
+df_er = df_sh[training_size:]
+
+T1 = Tree(df_tr, target="writing score", max_depth=2)
+out = []
+for depth in range(1, 10):
+
+	T1 = Tree(df_tr, target="writing score", max_depth=depth)
+	S = 0
+	for i in range(df_er.shape[0]):
+		pred = T1.Predict(df_er[i])
+		if pred == df_er.row(i)[-1]:
+			S += 1
+		else:
+			continue
+
+	accuracy = S/df_er.shape[0]
+	out.append(accuracy)
+
+
+plt.plot(out)
+plt.show()
+
+
 x=1
