286f71c79ae5691f27ba2a058835c01155b351a3
Casperbkkr <casperbkkr@gmail.com>
2023-03-14 12:05:52 +0100

    Can now make a single split. Next will start work on stopping criterium

--- Main.py
+++ Main.py
@@ -3,12 +3,12 @@ import pandas as pd
 import polars as pl
 from Tree import Tree
 df = pl.read_csv('iris.csv')
+df.drop_in_place('Id')
+T1 = Tree(df, 'Species')
+c = T1._Possible_splits_row(T1.df, 'SepalWidthCm', 'Species', T1._Average_entropy)
 
-T = Tree(df, 'Species')
-
-a = T._Entropy(T.df, 'Species')
-b = T._Average_entropy(T.df, T.df, 'Species')
-c = T._Possible_splits_col(T.df, 'SepalLengthCm')
-print(b)
-
+T2 = Tree(df, 'PetalLengthCm')
+d = T2._Possible_splits_row(T2.df, 'SepalLengthCm', 'PetalLengthCm', T2._Average_variance)
 x=1
+
+a = T2._Split(T2.df, "Species", T2._Average_entropy)
--- Tree.py
+++ Tree.py
@@ -2,15 +2,26 @@
 from Node import Node
 import polars as pl
 import math as mt
+
 class Tree:
-    def __init__(self, df, target) -> None:
+    def __init__(self, df, target, *, depth=0, root=None) -> None:
         self.df = df
         self.target = target
+        self.Root = None
+        self.branch_left = None
+        self.branch_right = None
+        self.Depth = depth
+        self.thresh = 0
 
+    def _Split(self, df, target, split_criteria) -> None:
+        z, df_left, df_right, self.thresh = \
+            self._Best_split(df, target, split_criteria)
 
-    def _Split(self) -> None:
-
-        pass
+        # make a new tree with on df
+        self.branch_left = Tree(df_left, self.target, depth=self.Depth + 1, root=self)
+        # make the other tree with another df
+        self.branch_right = Tree(df_right, self.target, depth=self.Depth + 1, root=self)
+        return
 
     def _Entropy(self, df, target):
         # count occurrence of different categories in df and divide by total size to get pi of category
@@ -25,55 +36,61 @@ class Tree:
         entropy = (-1) * sum([plogp(pi) for pi in P])
         return entropy
 
-    def _Average_entropy(self, df1, df2, target):
-        n_parent = df1.shape[0] + df2.shape[0]
+
+    def _Average_entropy(self, df1, df2, n_parent, target):
         weight1 = df1.shape[0]/n_parent
         weight2 = df2.shape[0]/n_parent
         avg_ent = weight1*self._Entropy(df1, target) + weight2*self._Entropy(df2, target)
         return avg_ent
 
-    def _Calc_pi(self, df):
-        return
 
     def _Variance(self, df):
         target = self.target
         return df.select(target).var()[0, 0]
 
-    def _Average_variance(self, df1, df2, n_splits):
-        # calculate variance of the splitted dataset
-        var1 = list(map(self._Variance, df1))
-        var12 = [x if x is not None else 0 for x in var1]
-        weight1 = [split.shape[0] / n_splits for split in df1]
+
+    def _Average_variance(self, df1, df2, n_parent, target):
+
+        # calculate variance of the split dataset
+        var1 = self._Variance(df1)
+        if var1 == None: Var1 = 0
+        weight1 = df1.shape[0]/n_parent
+
+        # calculate variance of its complement
+        var2 = self._Variance(df2)
+        if var2 == None: var2 = 0
+        weight2 = df2.shape[0]/n_parent
+
+        return weight1 * var1 + weight2 * var2
 
 
-    def _Possible_splits_col(self, df, column, split_criteria=1):
+    def _Possible_splits_row(self, df, column, target, split_criteria):
         n_splits = df.shape[0]
         # look up all possible data points to split over
         split_value = [df.select(column)[i][0, 0] for i in range(n_splits)]
 
         # create all splitted dataframes.
-        splitted = [df.filter(pl.col(column) <= val) for val in split_value]
-        splitted_comp = [df.filter(pl.col(column) > val) for val in split_value]
+        split = [df.filter(pl.col(column) <= val) for val in split_value]
+        split_comp = [df.filter(pl.col(column) > val) for val in split_value]
 
-        # calculate variance of the splitted dataset
-        var1 = list(map(self._Variance, splitted))
-        var12 = [x if x is not None else 0 for x in var1]
-        weight1 = [split.shape[0]/n_splits for split in splitted]
+        split_f = lambda i: split_criteria(split[i], split_comp[i], n_splits, target)
+        weighted_splits = [split_f(i) for i in range(len(split))]
 
-        # calculate variance of its complement
-        var2 = list(map(self._Variance, splitted_comp))
-        var22 = [x if x is not None else 0 for x in var2]
-        weight2 = [split.shape[0]/n_splits for split in splitted_comp]
-
-        # zip together for processing
-        zipped = zip(weight1, var12, weight2, var22)
-
-        # calculate the weighted variance
-        weighted_var = [w1*v1 + w2*v2 for w1, v1, w2, v2 in zipped]
         # find minimum weighted variance
-        min_var = min(weighted_var)
-        index = weighted_var.index(min_var)
+        min_split = min(weighted_splits)
+        index = weighted_splits.index(min_split)
 
-        return min_var, splitted[index], splitted_comp[index]
+        return min_split, split[index], split_comp[index], split_value[index]
 
+    def _Best_split(self, df, target, split_criteria):
+        n_attr = df.shape[1] - 1
+        attr_keys = list(df.schema.keys())[0:-1]
+
+        f_split = lambda col: self._Possible_splits_row(df, col, target, split_criteria)
+        possible_splits = [f_split(key) for key in attr_keys]
+        split_val = [possible_splits[i][0] for i in range(len(possible_splits))]
+        min_split_val = min(split_val)
+        min_split_ind = split_val.index(min_split_val)
+
+        return possible_splits[2]
 
